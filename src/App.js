88324bcbe56f12c339a79e2adedd45cca8949781
gabrielccarvalho <caampos22@gmail.com>
2020-03-24 16:19:51 -0300

    chore: add global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <GlobalStyle />
   );
 }
 
